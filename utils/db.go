dca06d407515eb18d1f1b842699b7f367cba5d2a
半城风雨 <1260211623@qq.com>
2021-11-19 14:44:09 +0800

    新增版权说明

--- utils/db.go
+++ utils/db.go
@@ -1,5 +1,22 @@
+// +----------------------------------------------------------------------
+// | EasyGoAdmin敏捷开发框架 [ EasyGoAdmin ]
+// +----------------------------------------------------------------------
+// | 版权所有 2019~2021 EasyGoAdmin深圳研发中心
+// +----------------------------------------------------------------------
+// | 官方网站: http://www.easygoadmin.vip
+// +----------------------------------------------------------------------
+// | Author: 半城风雨 <easygoadmin@163.com>
+// +----------------------------------------------------------------------
+// | 免责声明:
+// | 本软件框架禁止任何单位和个人用于任何违法、侵害他人合法利益等恶意的行为，禁止用于任何违
+// | 反我国法律法规的一切平台研发，任何单位和个人使用本软件框架用于产品研发而产生的任何意外
+// | 、疏忽、合约毁坏、诽谤、版权或知识产权侵犯及其造成的损失 (包括但不限于直接、间接、附带
+// | 或衍生的损失等)，本团队不承担任何法律责任。本软件框架只能用于公司和个人内部的法律所允
+// | 许的合法合规的软件产品研发，详细声明内容请阅读《框架免责声明》附件；
+// +----------------------------------------------------------------------
+
 /**
- *
+ * 数据库连接工具
  * @author 半城风雨
  * @since 2021/9/8
  * @File : db
@@ -7,6 +24,7 @@
 package utils
 
 import (
+	"easygoadmin/library/cfg"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
@@ -17,12 +35,24 @@ var XormDb *xorm.Engine
 
 func init() {
 	fmt.Println("初始化并连接数据库")
-	// 1.连接数据库
-	sqlStr := "root:@tcp(127.0.0.1:3306)/easygoadmin.gin.ele?charset=utf8&parseTime=true&loc=Local"
+
+	// 获取配置实例
+	config := cfg.Instance()
 	var err error
-	XormDb, err = xorm.NewEngine("mysql", sqlStr)
+	XormDb, err = xorm.NewEngine("mysql", config.Database.Master)
 	if err != nil {
-		fmt.Println("连接数据库失败", err)
+		fmt.Printf("数据库连接错误:%v", err.Error())
+		return
+	}
+
+	// 通过engine.Ping()来进行数据库的连接测试是否可以连接到数据库。
+	err = XormDb.Ping()
+	if err == nil {
+		fmt.Println("数据库连接成功")
+		//关闭连接
+		//defer XormDb.Close()
+	} else {
+		fmt.Printf("数据库连接错误:%v", err.Error())
 		return
 	}
 
@@ -30,7 +60,9 @@ func init() {
 	tbMapper := core.NewPrefixMapper(core.SnakeMapper{}, "sys_")
 	XormDb.SetTableMapper(tbMapper)
 
-	// 会在控制台打印执行的sql
-	XormDb.ShowSQL(true)
-	XormDb.Logger().SetLevel(core.LOG_DEBUG)
+	// 开启调试模式和打印日志,会在控制台打印执行的sql
+	if cfg.Instance().Database.Debug {
+		XormDb.ShowSQL(cfg.Instance().Database.Debug)
+		XormDb.Logger().SetLevel(core.LOG_DEBUG)
+	}
 }
